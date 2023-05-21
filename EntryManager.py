44a7020351b2107db3d9308f3bc5dc69233fe7bd
gmturn <turnermclean2402@gmail.com>
2023-05-21 08:59:14 -0500

    Updates - not tested

--- EntryManager.py
+++ EntryManager.py
@@ -71,40 +71,91 @@ class EntryManager:
             raise ValueError(
                 "Error: EntryManager.addEntry(entry) value is not type Entry.")
 
-        entryDirectory = "./data/Entries/"
         startDateTime = entry.getStartTime()
         endDateTime = entry.getEndTime()
-        activityName = entry.getActivityName()
         currentDateTime = startDateTime
 
         while currentDateTime < endDateTime:
-            # End of current day or endDateTime, whichever is sooner
-            nextDateTime = min(currentDateTime.replace(
-                hour=23, minute=59, second=59, microsecond=999999), endDateTime)
-
-            # Calculate duration for current day
+            nextDateTime = self.get_end_of_day_or_entry(
+                currentDateTime, endDateTime)
             duration = nextDateTime - currentDateTime
-            entryFile = f"{entryDirectory}{currentDateTime.strftime('%Y-%m-%d')}.json"
-
-            # Create the entry for the current day
-            entryData = {
-                "activity": activityName,
-                "start": str(currentDateTime),
-                "end": str(nextDateTime),
-                "duration": str(duration)
-            }
-
-            # If file for the day doesn't exist, create it and initialize with the current entry
-            if not os.path.exists(entryFile):
-                with open(entryFile, 'w') as file:
-                    json.dump({"entries": [entryData]}, file, indent=2)
-            else:
-                # If file for the day exists, append the entry to it
-                with open(entryFile, 'r') as file:
-                    data = json.load(file)
-                data["entries"].append(entryData)
-                with open(entryFile, 'w') as file:
-                    json.dump(data, file, indent=2)
-
-            # Move to the next day
+            entryFile = self.get_entry_file_path(currentDateTime)
+            entryData = self.create_entry_data(
+                entry, currentDateTime, nextDateTime, duration)
+            data = self.load_or_create_data(entryFile, entryData)
+            data = self.update_stats(data, entry, duration)
+            self.save_to_file(entryFile, data)
             currentDateTime += datetime.timedelta(days=1)
+
+    def get_end_of_day_or_entry(self, currentDateTime, endDateTime):
+        return min(currentDateTime.replace(hour=23, minute=59, second=59, microsecond=999999), endDateTime)
+
+    def get_entry_file_path(self, currentDateTime):
+        entryDirectory = "./data/Entries/"
+        return f"{entryDirectory}{currentDateTime.strftime('%Y-%m-%d')}.json"
+
+    def create_entry_data(self, entry, startDateTime, endDateTime, duration):
+        return {
+            "activity": entry.getActivityName(),
+            "start": str(startDateTime),
+            "end": str(endDateTime),
+            "duration": str(duration)
+        }
+
+    def load_or_create_data(self, entryFile, entryData):
+        if os.path.exists(entryFile):
+            with open(entryFile, 'r') as file:
+                data = json.load(file)
+            data["entries"].append(entryData)
+        else:
+            data = {"entries": [entryData], "stats": self.initialize_stats()}
+        return data
+
+    def initialize_stats(self):
+        return {
+            "totalActivities": 0,
+            "totalDuration": "00:00:00",
+            "uniqueActivities": [],
+            "activityCounts": {},
+            "activityDurations": {}
+        }
+
+    def update_stats(self, data, entry, duration):
+        data["stats"]["totalActivities"] += 1
+        # You can create methods to update the total duration and activity data
+        data["stats"]["totalDuration"] = self.update_total_duration(
+            data["stats"]["totalDuration"], duration)
+        data["stats"] = self.update_activity_data(
+            data["stats"], entry.getActivityName(), duration)
+        return data
+
+    def save_to_file(self, entryFile, data):
+        with open(entryFile, 'w') as file:
+            json.dump(data, file, indent=2)
+
+    def update_total_duration(self, totalDuration, duration):
+        # totalDuration is a string in the format "HH:MM:SS"
+        total_seconds = datetime.timedelta(hours=int(totalDuration.split(':')[0]),
+                                           minutes=int(
+                                               totalDuration.split(':')[1]),
+                                           seconds=int(totalDuration.split(':')[2])).total_seconds()
+        total_seconds += duration.total_seconds()
+        updated_duration = datetime.timedelta(seconds=total_seconds)
+
+        # Convert updated_duration back to "HH:MM:SS" format
+        hours, remainder = divmod(updated_duration.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+    def update_activity_data(self, stats, activityName, duration):
+        if activityName not in stats["uniqueActivities"]:
+            stats["uniqueActivities"].append(activityName)
+            stats["activityCounts"][activityName] = 1
+            stats["activityDurations"][activityName] = str(duration)
+        else:
+            stats["activityCounts"][activityName] += 1
+            current_activity_duration = stats["activityDurations"][activityName]
+            updated_activity_duration = self.update_total_duration(
+                current_activity_duration, duration)
+            stats["activityDurations"][activityName] = updated_activity_duration
+        return stats
--- tempFile.py
+++ tempFile.py
@@ -3,10 +3,12 @@ import datetime
 import EntryManager
 
 startDateTime = datetime.datetime(2023, 5, 20, 12, 0, 0)
-endDateTime = datetime.datetime(2023, 5, 20, 13, 15, 0)
+endDateTime = datetime.datetime(2023, 5, 20, 13, 30, 0)
 
 
-myEntry = Entry("Weight Lifting", startDateTime, endDateTime)
+# myEntry = Entry("Weight Lifting", startDateTime, endDateTime)
+myEntry2 = Entry("Weight Lifting", startDateTime, endDateTime)
 
 
 myManager = EntryManager.EntryManager()
+myManager.addEntry(myEntry2)
