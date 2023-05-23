0b70f8ba9cb724d83ea5d3989405da821770523f
gmturn <turnermclean2402@gmail.com>
2023-05-22 21:40:48 -0500

    Added formatting updates to Add Entry tab

--- gui/entryManagerFrame.py
+++ gui/entryManagerFrame.py
@@ -13,11 +13,11 @@ class EntryManagerFrame(ctk.CTkFrame):
         self.mainTabView = ctk.CTkTabview(self, width=500)
 
         # Setting frame grid size, not too sure how it works
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
-        self.grid_columnconfigure(3, weight=1)
-        self.grid_columnconfigure(4, weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(4, weight=1)
 
         self.mainTabView.add("Add Entry")
         self.mainTabView.add("Tab 2")
@@ -26,6 +26,10 @@ class EntryManagerFrame(ctk.CTkFrame):
 
         self.mainTabView.grid(row=0, column=0, padx=(
             20, 0), pady=(20, 0), sticky="NESW")  # sets location of the entire tab
+
+        self.mainTabView.grid_columnconfigure(0, weight=1)
+        self.mainTabView.grid_columnconfigure(1, weight=1)
+        self.mainTabView.grid_columnconfigure(2, weight=1)
 
         mainLabel = ctk.CTkLabel(
             self.mainTabView.tab("Add Entry"), text="Add an Entry", font=ctk.CTkFont(size=20, weight="bold"))
