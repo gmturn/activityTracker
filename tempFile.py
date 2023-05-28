52f86ec3fc33378cc04b5c6877f44d032824308e
gmturn <turnermclean2402@gmail.com>
2023-05-28 10:46:20 -0500

    updates

--- tempFile.py
+++ tempFile.py
@@ -2,11 +2,11 @@ from Models.EntryModel import Entry
 import datetime
 import managers.EntryManager as EntryManager
 
-startDateTime = datetime.datetime(2023, 5, 28, 2, 0, 0)
-endDateTime = datetime.datetime(2023, 5, 28, 4, 30, 0)
+startDateTime = datetime.datetime(2023, 5, 27, 11, 0, 0)
+endDateTime = datetime.datetime(2023, 5, 27, 13, 30, 0)
 
 
-myEntry2 = Entry("Coding", startDateTime, endDateTime)
+myEntry2 = Entry("Weight Lifting", startDateTime, endDateTime)
 
 
 myManager = EntryManager.EntryManager()
